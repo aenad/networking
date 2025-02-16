92c8ede9e52143442270d4038a212b5bcad052b8
Atefe Naderi <ae.naderi@gmail.com>
2025-02-16 20:03:02 +0330

    multi thread server

--- client.py
+++ client.py
@@ -2,12 +2,27 @@ import socket
 
 HOST = "127.0.0.1"
 PORT = 65432
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as S:
-    S.connect((HOST, PORT))
-
-    S.sendall(b"hello, world")
-    data = S.recv(1024)
+SIZE = 1024
+FORMAT = "utf-8"
+DISCONNECT_MSG = "!DISCONNECT"
 
 
-print(f"Received {data}")
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.connect((HOST, PORT))
+        print(f"[CONNECTED] Client connected to server {HOST}:{PORT}")
+
+        connected = True
+        while connected:
+            msg = input("> ")
+            client.send(msg.encode(FORMAT))
+
+            if msg == DISCONNECT_MSG:
+                connected = False
+            else:
+                msg = client.recv(SIZE).decode(FORMAT)
+                print(f"[SERVER] {msg}")
+
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,19 +1,47 @@
 import socket
+import threading
+from celery import Celery
 
 HOST = "127.0.0.1"
-
 PORT = 65432
+SIZE = 1024
+FORMAT = "utf-8"
+DISCONNECT_MSG = "!DISCONNECT"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
 
-    with conn:
-        print(f"connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected")
+
+    connected = True
+    while connected:
+        try:
+            msg = conn.recv(SIZE).decode(FORMAT)
+            if not msg:
                 break
-            conn.sendall(data)
-            
+            if msg == DISCONNECT_MSG:
+                connected = False
+            print(f"[{addr}] {msg}")
+            msg = f"Msg received: {msg}"
+            conn.send(msg.encode(FORMAT))
+        except ConnectionResetError:
+            print(f"[ERROR] Connection reset by {addr}")
+            break
+    conn.close()
+
+
+def main():
+    print("[Starting] Server is starting...")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        server.bind((HOST, PORT))
+        server.listen()
+        print(f"[LISTENING] Server is listening on {HOST}:{PORT}")
+
+        while True:  # accept multiple connection
+            conn, addr = server.accept()
+            thread = threading.Thread(target=handle_client, args=(conn, addr))
+            thread.start()
+            print(f"[Active Connections] {threading.active_count()}")
+
+
+if __name__ == "__main__":
+    main()
